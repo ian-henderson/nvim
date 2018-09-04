52ab0d41067f75194044036145100e1f719f3d50
Ian Henderson <iancurtish@gmail.com>
2018-09-04 13:05:39 -0500

    install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,14 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-for git_url in $@; do
-  # TODO: extract repo_name from url
-  # git submodule add $git_url ./bundle/$repo_name
+for url in $@; do
+  if [[ $url != *.git ]]; then
+    echo Invalid git url: $url
+    exit 1
+  fi
+done
+
+for url in $@; do
+  plugin_name=$(basename -- $url .git)
+  git submodule add $url ./bundle/$plugin_name
 done
